f4acbb009a9db15328832bf1768096a8085ad4d9
magdalenabegic <mabegic1@vernnet.hr>
2024-01-28 23:03:07 +0100

    edit styles

--- api/predmeti.js
+++ api/predmeti.js
@@ -1,3 +1,5 @@
+// Primjer za API ruta /api/studenti/predmeti
+
 const express = require("express");
 const router = express.Router();
 
@@ -7,17 +9,37 @@ router.get("/predmeti", async (req, res) => {
   }
 
   try {
-    const rows = await req.DB.all(
-      `SELECT predmeti.id, predmeti.naziv FROM predmeti
-      LEFT JOIN predmeti_studenti ON predmeti.id = predmeti_studenti.predmet_id
-      WHERE predmeti_studenti.student_id = ?`,
-      [req.session.data.id]
-    );
+    const rows = await new Promise((resolve, reject) => {
+      req.DB.all(
+        `
+        SELECT 
+          predmeti.id AS predmet_id, 
+          predmeti.naziv AS predmet_naziv, 
+          profesori.id AS profesor_id, 
+          profesori.ime AS profesor_ime, 
+          profesori.prezime AS profesor_prezime 
+        FROM predmeti
+        LEFT JOIN predmeti_studenti ON predmeti.id = predmeti_studenti.predmet_id
+        LEFT JOIN profesori ON predmeti.profesor_id = profesori.id
+        WHERE predmeti_studenti.student_id = ?
+        `,
+        [req.session.data.id],
+        (error, rows) => {
+          if (error) {
+            return reject(error);
+          }
+          return resolve(rows);
+        }
+      );
+    }).catch((e) => {
+      console.error(e);
+      return null;
+    });
 
     return res.json(rows);
   } catch (error) {
-    console.error("Error fetching subjects:", error);
-    return res.status(500).json({ error: "Internal Server Error" });
+    console.error("Error fetching subjects with professors:", error);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
